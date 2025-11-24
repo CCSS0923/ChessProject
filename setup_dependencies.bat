42c21c6e7594e1edb8e2354304be3c1ceea11427
CCSS0923 <dmltjd4588@gmail.com>
2025-11-24 17:51:02 +0900

    Dependency isolation

--- setup_dependencies.bat
+++ setup_dependencies.bat
@@ -1,13 +1,34 @@
 @echo off
-echo Downloading dependencies...
+echo ===== Downloading dependencies =====
 
-REM Stockfish
-curl -L -o stockfish.zip https://stockfishchess.org/files/stockfish-17-win-x86-64.zip
-tar -xf stockfish.zip -C extern/stockfish
+REM ---------- prepare folders ----------
+if not exist extern mkdir extern
+if not exist extern\stockfish mkdir extern\stockfish
+if not exist extern\opencv mkdir extern\opencv
 
-REM OpenCV
-curl -L -o opencv.zip https://github.com/opencv/opencv/releases/download/4.12.0/opencv-4.12.0-windows.zip
-tar -xf opencv.zip -C extern/opencv
+REM ---------- Stockfish ----------
+set STOCK_URL=https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-windows-x86-64-avx2.zip
+set STOCK_ZIP=stockfish.zip
 
-echo Done.
+echo [1/2] Downloading Stockfish...
+curl -L -o "%STOCK_ZIP%" "%STOCK_URL%"
+
+echo Extracting Stockfish...
+powershell -command "Expand-Archive -Force '%STOCK_ZIP%' 'extern'"
+del "%STOCK_ZIP%"
+
+REM ---------- OpenCV ----------
+set OPENCV_URL=https://github.com/opencv/opencv/releases/download/4.12.0/opencv-4.12.0-windows.exe
+set OPENCV_EXE=opencv.exe
+
+echo [2/2] Downloading OpenCV...
+curl -L -o "%OPENCV_EXE%" "%OPENCV_URL%"
+
+echo Extracting OpenCV...
+"%OPENCV_EXE%" -o extern/opencv
+
+del "%OPENCV_EXE%"
+
+echo.
+echo ===== DONE =====
 pause
